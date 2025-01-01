cb3f2d59f0c8c767cbb9bb18a4ccb6f370418f78
Muller Marciel <mullermbm@gmail.com>
2025-01-01 17:05:40 -0300

    alteracao do caminho das imagens utilizando o basePath

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,15 @@
 import Image from "next/image";
 import './globals.css';
+import { useRouter } from "next/router";
 
 export default function Home() {
+  const { basePath } = useRouter();
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <Image
           className="dark:invert"
-          src="/next.svg"
+          src={`${basePath}/next.svg`}
           alt="Next.js logo"
           width={180}
           height={38}
@@ -33,7 +35,7 @@ export default function Home() {
           >
             <Image
               className="dark:invert"
-              src="/vercel.svg"
+              src={`${basePath}/vercel.svg`}
               alt="Vercel logomark"
               width={20}
               height={20}
